86276566506db1b169885cab3e3d78b8988fa5d1
allensze <allen.sze@digipen.edu>
2024-06-28 16:32:13 -0700

    Pause Menu
    Pause functionality, tab function for the species add to 'Speciapedia'

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject gameMenu;
+
+    public static bool isPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        gameMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        gameMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
